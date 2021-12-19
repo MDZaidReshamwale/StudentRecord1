969e09b0305119ce0e03e6cc238fcc805ffd91e2
msre0001 <zaidreshamwale2015@gmail.com>
2021-12-19 23:18:23 +0530

    - Change in StudentActivity the thread is added and intent

--- app/src/main/java/com/zaid/studentrecord/activity/StudentActivity.java
+++ app/src/main/java/com/zaid/studentrecord/activity/StudentActivity.java
@@ -2,7 +2,9 @@ package com.zaid.studentrecord.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.zaid.studentrecord.R;
 
@@ -12,5 +14,24 @@ public class StudentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+    }
+
+    Thread th = new Thread(
+            new Runnable() {
+                @Override
+                public void run() {
+                    try{
+                        Thread.sleep(200);
+                        startActivity(new Intent(StudentActivity.this, AddStudentActivity.class));
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+    );
+
+    public void Next(View view) {
+        Intent i = new Intent(this, AddStudentActivity.class);
+        startActivity(i);
     }
 }
