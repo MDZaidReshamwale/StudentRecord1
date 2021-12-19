18121363af67489506ab717462428674a17cb626
msre0001 <zaidreshamwale2015@gmail.com>
2021-12-19 22:38:50 +0530

    -	Uncheck the Compact middleware package -	Create package in \app\src\main\java\com\zaid\studentrecord 1.	Activity 2.	Adapter 3.	Db 4.	Model -	Check back the Compact middleware package -	Create an empty activity (StudentActivity) in studentrecord\activity and make it launcher activity -	Create another activity called (empty activity) in studentrecord\activity 1.	AddStudentActivity 2.	StudentListActivity 3.	UpdateStudentActivity

---
+++ app/src/main/java/com/zaid/studentrecord/activity/StudentActivity.java
@@ -0,0 +1,16 @@
+package com.zaid.studentrecord.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zaid.studentrecord.R;
+
+public class StudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+    }
+}
---
+++ app/src/main/java/com/zaid/studentrecord/activity/UpdateStudentActivity.java
@@ -0,0 +1,16 @@
+package com.zaid.studentrecord.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zaid.studentrecord.R;
+
+public class UpdateStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_student);
+    }
+}
