a7ec1acc73b73ab3986452eb3d22e07b0d6a7182
msre0001 <zaidreshamwale2015@gmail.com>
2021-12-20 01:35:24 +0530

    - Created Student.java in model - Edit the AddStudentActivity.java - Edited the DbHelper.java - Edited the  StudentListActivity.java

--- app/src/main/java/com/zaid/studentrecord/activity/StudentListActivity.java
+++ app/src/main/java/com/zaid/studentrecord/activity/StudentListActivity.java
@@ -19,6 +19,7 @@ public class StudentListActivity extends AppCompatActivity {
     TextView tvTotal;
     RecyclerView recyclerView;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list);
