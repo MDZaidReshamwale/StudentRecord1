b2a95016ef7b1b3eb0bc3f47b56286459033b8d8
msre0001 <zaidreshamwale2015@gmail.com>
2021-12-19 23:31:55 +0530

    - Change in AddStudent.xml file

--- app/src/main/java/com/zaid/studentrecord/activity/AddStudentActivity.java
+++ app/src/main/java/com/zaid/studentrecord/activity/AddStudentActivity.java
@@ -3,6 +3,7 @@ package com.zaid.studentrecord.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.zaid.studentrecord.R;
 
@@ -12,5 +13,11 @@ public class AddStudentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+    }
+
+    public void save(View view) {
+    }
+
+    public void showAll(View view) {
     }
 }
